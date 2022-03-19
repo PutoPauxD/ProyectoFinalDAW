d8136ba8af4e5682be317b6d4051bbe7679ea416
Pau Higon Martinez <pauhigon.alumno@gmail.com>
2022-03-19 20:32:27 +0100

    Estructura y Routing Profile

---
+++ AppWeb/src/app/profile/profile.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './profile.component';
+import { ProfileHeaderComponent } from './profile-header/profile-header.component';
+import { PostsComponent } from './posts/posts.component';
+import { SharedPostsComponent } from './shared-posts/shared-posts.component';
+import { MediaComponent } from './media/media.component';
+import { RouterModule, Routes } from '@angular/router';
+
+export const rutasHijas: Routes = [
+  {path: '', component: ProfileComponent, children: [
+    {path: '', component: PostsComponent},
+    {path: 'media', component: MediaComponent},
+    {path: 'shared', component: SharedPostsComponent},
+  ]},
+]
+
+
+@NgModule({
+  declarations: [
+    ProfileHeaderComponent,
+    ProfileComponent,
+    PostsComponent,
+    SharedPostsComponent,
+    MediaComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(rutasHijas)
+  ],
+  exports: []
+
+})
+export class ProfileModule { }
---
+++ AppWeb/src/app/profile/shared-posts/shared-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-posts',
+  templateUrl: './shared-posts.component.html',
+  styleUrls: ['./shared-posts.component.css']
+})
+export class SharedPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
