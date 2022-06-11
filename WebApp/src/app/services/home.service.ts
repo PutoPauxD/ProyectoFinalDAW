b5a82b33b7e34d50090c76a078dc883a42162df6
Pau Higon Martinez <pauhigon.alumno@gmail.com>
2022-06-11 12:11:27 +0200

    Version final con url servicios

--- WebApp/src/app/services/home.service.ts
+++ WebApp/src/app/services/home.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class HomeService {
 
-  private url = 'http://localhost:3000/api/home'
+  private url = 'http://192.168.168.153:3000/api/home'
 
   constructor(private http: HttpClient) { }
 
